16757fef8e350709fe1273c8eb883930a05b6ba5
Owez <owez@scalist.net>
2019-11-09 21:02:45 +0000

    Moved themes into a /data folder for future uses Added new data to ci

--- src/main.rs
+++ src/main.rs
@@ -21,7 +21,7 @@ fn main() {
 
     let mut siv = Cursive::default();
 
-    siv.load_theme_file("themes/dark.toml").unwrap(); // Dark default theme
+    siv.load_theme_file("data/themes/dark.toml").unwrap(); // Dark default theme
 
     siv.add_global_callback('e', |s| s.quit());
     zeno_launch(&mut siv, meta);
