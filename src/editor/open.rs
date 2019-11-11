a8709b8b5ac6c53d132d07f70442e7a5bc86d9a1
scOwez <owez@scalist.net>
2019-11-11 12:17:19 +0000

    Finished file opening & tested.

--- src/editor/open.rs
+++ src/editor/open.rs
@@ -1,5 +1,5 @@
 //! # About
-//! 
+//!
 //! Contains utility functions for reading contents of files and an interactive
 //! cursive prompt to open a new file inside of Zeno, instead of using the usual
 //! CLI.
@@ -9,7 +9,6 @@ use cursive::{traits::*, Cursive};
 use std::fs::File;
 use std::io::prelude::*;
 use std::path::PathBuf;
-use std::rc::Rc;
 
 /// Displays a popup to open a file and adds it to the primary text area ID (`tb`).
 pub fn open_path_str(s: &mut Cursive) {
@@ -17,31 +16,35 @@ pub fn open_path_str(s: &mut Cursive) {
         Dialog::around(EditView::new().with_id("open_file_name").fixed_width(32))
             .title("Open file")
             .button("Open", |s| {
-                let file_path = PathBuf::from(
-                    Rc::try_unwrap(
-                        s.call_on_id("open_file_name", |view: &mut EditView| view.get_content())
-                            .unwrap(),
-                    )
-                    .unwrap(),
-                ); // TODO fix
+                let got_path = s
+                    .call_on_id("open_file_name", |view: &mut EditView| view.get_content())
+                    .unwrap(); // Get path from EditView
 
-                if !file_path.exists() {
-                    s.add_layer(Dialog::info(format!("{:?} does not exist", file_path)));
-                    s.pop_layer();
-                } else if file_path.is_dir() {
-                    s.add_layer(Dialog::info(format!("{:?} is a directory", file_path)));
-                    s.pop_layer();
-                }
+                let contents = get_content_str(s, &got_path); // Use [got_path] to get file contents
 
-                s.call_on_id("tb", |view: &mut TextArea| {
-                    view.set_content(get_path_content(&file_path))
-                })
-                .unwrap();
+                s.call_on_id("tb", |view: &mut TextArea| view.set_content(contents))
+                    .unwrap(); // Set textarea content
             })
             .button("Cancel", |s| {
                 s.pop_layer();
             }),
     );
+}
+
+/// Gets content from a given &[str] and return file's contents or nicely error
+/// when trying.
+fn get_content_str(s: &mut Cursive, file_path: &str) -> String {
+    let pb_p = PathBuf::from(file_path);
+
+    if !pb_p.exists() {
+        s.add_layer(Dialog::info(format!("{:?} does not exist", file_path)));
+        s.pop_layer();
+    } else if pb_p.is_dir() {
+        s.add_layer(Dialog::info(format!("{:?} is a directory", file_path)));
+        s.pop_layer();
+    }
+
+    get_path_content(&pb_p)
 }
 
 /// Gets content from a specified file path ([PathBuf]) and returns a string
