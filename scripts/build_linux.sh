30bddca17f96ee64c2696a0ce8ae40be65411137
scOwez <owez@scalist.net>
2019-11-27 13:59:20 +0000

    Simple linux builder

---
+++ scripts/build_linux.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+echo "Installing.."
+
+git clone https://gitlab.com/zeno-src/zeno
+mv ./zeno/ zeno-build/
+cd zeno-build/
+cargo build --release
+cd ..
+mkdir zeno/
+mv ./zeno-build/target/release/zeno ./zeno/
+cp -r ./zeno-build/data/ ./zeno/
+rm -rf zeno-build/
+cd zeno/
+strip ./zeno
+chmod +x ./zeno
+
+echo "zeno='.$PWD/zeno'" >> ~/.bash_aliases && source ~/.bash_aliases
+
+echo "Installed @ './zeno/*'!"
